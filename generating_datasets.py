f9765720ca86995285a08aedb818c9b283a0a59f
Mikulas Plesak <mikulas.plesak@gmail.com>
2020-07-10 16:14:19 +0530

    Update generating_datasets.py

--- generating_datasets.py
+++ generating_datasets.py
@@ -11,21 +11,18 @@ from maccabee.parameters import build_default_parameters
 
 HIGH, MEDIUM, LOW = Constants.AxisLevels.HIGH, Constants.AxisLevels.MEDIUM, Constants.AxisLevels.LOW
 
-# iterate over all possible settings of a parameter
 for i in [HIGH, MEDIUM, LOW]:
     dgp_params = build_parameters_from_axis_levels({
-        Constants.AxisNames.OVERLAP: i,                   # choose desirable parameter to manipulate here
-        Constants.AxisNames.PERCENT_TREATED: LOW          # LOW defaults to 35% - use the next line to specify
+        Constants.AxisNames.TE_HETEROGENEITY: i
     })
     
-    # dgp_params.set_parameter("TARGET_PROPENSITY_SCORE", 0.8)
+    dgp_params.set_parameter("TARGET_PROPENSITY_SCORE", 0.05)
     
     # pprint(vars(dgp_params))
     
     normal_data_source = build_random_normal_datasource(
-        n_covars=5,
-        n_observations=5000)
-    
+        n_covars=3,
+        n_observations=1000)
     # pprint(vars(normal_data_source))
 
 
@@ -39,7 +36,4 @@ for i in [HIGH, MEDIUM, LOW]:
     # pprint(vars(sampled_dgp))
     sampled_dataset = sampled_dgp.generate_dataset()
     
-    # use this line to display the generated dataset in python
-    # sampled_dataset.observed_data 
-    # use this line to save the generated dataset to file; use string formatting if you're saving multiple files in a single loop
-    # sampled_dataset.observed_data.to_csv("data_overlap_{}.csv".format(i))
+    sampled_dataset.observed_data.to_csv("trt_hetero_{}.csv".format(i))
